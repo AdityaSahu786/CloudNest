cb118940dd624222fff08cb421f20fc51c5ac9b6
AdityaSahu786 <sahuadi786183@gmail.com>
2024-11-19 01:01:47 +0530

    Reusable AuthForm

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const Layout = ({children}: { children: React.ReactNode }) => {
   return (
     <div className='flex min-h-screen'>
-        <section className="bg-brand p-10">
-            <div>
+        <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+            <div className='flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12'>
                 <Image 
-                src="/Logo.png"
+                src="/assets/icons/logo-full.svg"
                 alt="logo"
                 width={224}
                 height={82}
@@ -21,7 +21,7 @@ const Layout = ({children}: { children: React.ReactNode }) => {
                      </p>
                  </div>
                  <Image
-                   src="/Illustration.png"
+                   src="/assets/images/files.png"
                    alt="Files"
                    width={342}
                    height={342}
@@ -29,7 +29,21 @@ const Layout = ({children}: { children: React.ReactNode }) => {
                  />
             </div>
         </section>
-        {children}
+
+        
+
+         <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+          <div className='mb-16 lg:hidden'>
+             <Image 
+               src="/assets/icons/logo-full-brand.svg"
+               alt="logo"
+               width={224}
+               height={82}
+               className='h-auto w-[200px] lg:W-[250px]'
+               />
+          </div>
+          {children}
+         </section>
         </div>
   )
 }
