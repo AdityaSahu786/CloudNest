d3025d4c792f945f15dfaf33c48b25a2fee7a563
AdityaSahu786 <sahuadi786183@gmail.com>
2024-11-18 23:36:15 +0530

    Auth Layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,36 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Layout = () => {
+const Layout = ({children}: { children: React.ReactNode }) => {
   return (
-    <div>Layout</div>
+    <div className='flex min-h-screen'>
+        <section className="bg-brand p-10">
+            <div>
+                <Image 
+                src="/Logo.png"
+                alt="logo"
+                width={224}
+                height={82}
+                className="h-auto"
+                 />
+
+                 <div className='space-y-5 text-white'>
+                     <h1 className='h1'>Manage your files the best way</h1>
+                     <p className='body-1'>
+                          This is a place where you can store all your documents.
+                     </p>
+                 </div>
+                 <Image
+                   src="/Illustration.png"
+                   alt="Files"
+                   width={342}
+                   height={342}
+                   className='transition-all hover:rotate-2 hover:scale-105'
+                 />
+            </div>
+        </section>
+        {children}
+        </div>
   )
 }
 
