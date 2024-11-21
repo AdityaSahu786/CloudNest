278bd1369b5c275dbc0d49f4813037fdad63d49c
AdityaSahu786 <sahuadi786183@gmail.com>
2024-11-21 21:33:22 +0530

    Updating Reusable AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,9 @@ import {
   FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import  Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -22,7 +25,14 @@ const formSchema = z.object({
 
 type FormType = "sign-in" | "sign-up";
 
+const authFormSchema = (FormType) => {
+
+}
+
+
 const AuthForm = ({ type }: {type: FormType }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   
   const form = useForm<z.infer<typeof formSchema>>( {
     resolver: zodResolver(formSchema),
@@ -42,27 +52,79 @@ const AuthForm = ({ type }: {type: FormType }) => {
         <h1  className="form-title">
           { type ===  "sign-in" ? "Sign In" : "Sign Up" }
         </h1>
-          {type === 'sign-up' && ( <FormField
+          {type === 'sign-up' && ( 
+            <FormField
           control={form.control}
           name="fullName"
           render={({ field }) => (
             <FormItem>
                <div className="shad-form-item">
-               <FormLabel>Full Name</FormLabel>
+               <FormLabel className="shad-form-label">Full Name</FormLabel>
 
                <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="Enter your full name"
+                 className="shad-input"
+                {...field} />
               </FormControl>
                </div>
-             
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
+              <FormMessage className="shad-form-message"/>
             </FormItem>
           )}
-        />)}
-        <Button type="submit">Submit</Button>
+        />
+        )}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+               <div className="shad-form-item">
+               <FormLabel className="shad-form-label">Email</FormLabel>
+
+               <FormControl>
+                <Input placeholder="Enter your email"
+                 className="shad-input"
+                {...field} />
+              </FormControl>
+               </div>
+              <FormMessage className="shad-form-message"/>
+            </FormItem>
+          )}
+        />
+
+
+        <Button type="submit" className="form-submit-button" disabled={isLoading}>
+          {type === "sign-in" ? "Sign In" : "Sign Up"}
+
+          {isLoading && (
+            <Image 
+            src="/assets/icons/loader.svg" 
+            alt="loader" 
+            width={24} 
+            height={24} 
+            className="animate-sprint ml-2"
+             />
+          )}
+        </Button>
+
+        {errorMessage && 
+          <p className="error-message">*{errorMessage}</p>
+        }
+
+        <div className="body-2 flex justify-center">
+          <p className="text-light-100">
+            { type === "sign-in"
+              ? "Don't have an account?"
+              : "Already have an account?"
+              }
+          </p>
+          <Link
+            href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+            className="ml-1 font-medium text-brand"
+          >
+            {" "}
+            {type === "sign-in" ? "Sign Up" : "Sign In"}
+          </Link>
+        </div>
       </form>
     </Form>
      {/*OTP Verification */}
